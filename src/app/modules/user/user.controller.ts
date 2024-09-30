80a252a6cc633e0d940aee48280c4fc3dc31f52a
shvnsaha <shovan.css2018@gmail.com>
2024-09-30 23:56:46 +0600

    follow unfollow bookmark api done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -26,8 +26,87 @@ const getSingleUser = catchAsync(async (req, res) => {
     });
   });
 
+  const followUser = catchAsync(async (req, res) => {
+    const { followerId } = req.body;
+    const followingId = req.params.id;
+  
+    const result = await UserServices.followUser(followerId, followingId);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "User followed successfully",
+      data: result,
+    });
+  });
+  
+  const unfollowUser = catchAsync(async (req, res) => {
+    const { followerId } = req.body;
+    const followingId = req.params.id;
+  
+    const result = await UserServices.unfollowUser(followerId, followingId);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "User unfollowed successfully",
+      data: result,
+    });
+  });
+  
+  // favorite post
+  const bookmark = catchAsync(async (req, res) => {
+    const { userId } = req.body;
+    const postId = req.params.id;
+  
+    const result = await UserServices.bookmarkPost(userId, postId);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Post added to bookmarked",
+      data: result,
+    });
+  });
+  
+  // unfavorite post
+  const removeBookmark = catchAsync(async (req, res) => {
+    const { userId } = req.body;
+    const postId = req.params.id;
+  
+    const result = await UserServices.removeBookmarkPost(userId, postId);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Post removed from favorites successfully",
+      data: result,
+    });
+  });
+  
+  // get user favorites posts
+  const getUserFavoritesPosts = catchAsync(
+    async (req, res) => {
+      const userId = req.params.id;
+  
+      const result = await UserServices.getUserFavorites(userId);
+  
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "User favorites retrieved successfully",
+        data: result,
+      });
+    }
+  );
+
 
   export const UserControllers = {
     getSingleUser,
-    updateUser
+    updateUser,
+    followUser,
+    unfollowUser,
+    bookmark,
+    removeBookmark,
+    getUserFavoritesPosts
   }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,21 +2,79 @@ import { TUser } from "./user.interface";
 import { User } from "./user.model";
 
 const getSingleUserFromDB = async (email: string) => {
-    const result = await User.findOne({ email });
-    return result;
-  };
+  const result = await User.findOne({ email });
+  return result;
+};
 
-  const updateUserIntoDB = async (id: string, payload: Partial<TUser>) => {
-    const result = await User.findByIdAndUpdate(
-      id,
-      { $set: payload },
-      { new: true, runValidators: true }
-    ).select("-password");
-  
-    return result;
-  };
+const updateUserIntoDB = async (id: string, payload: Partial<TUser>) => {
+  const result = await User.findByIdAndUpdate(
+    id,
+    { $set: payload },
+    { new: true, runValidators: true }
+  ).select("-password");
 
-  export const UserServices = {
-    getSingleUserFromDB,
-    updateUserIntoDB
-  }
+  return result;
+};
+
+const followUser = async (followerId: string, followingId: string) => {
+  const result = await User.findByIdAndUpdate(
+    followerId,
+    { $addToSet: { followings: followingId } },
+    { new: true }
+  );
+
+  await User.findByIdAndUpdate(followingId, {
+    $addToSet: { followers: followerId },
+  });
+
+  return result;
+};
+
+const unfollowUser = async (followerId: string, followingId: string) => {
+  const result = await User.findByIdAndUpdate(
+    followerId,
+    { $pull: { followings: followingId } },
+    { new: true }
+  );
+
+  await User.findByIdAndUpdate(followingId, {
+    $pull: { followers: followerId },
+  });
+
+  return result;
+};
+
+const bookmarkPost = async (userId: string, postId: string) => {
+  const result = await User.findByIdAndUpdate(
+    userId,
+    { $addToSet: { bookmarks: postId } },
+    { new: true }
+  );
+
+  return result;
+};
+
+const removeBookmarkPost = async (userId: string, postId: string) => {
+  const result = await User.findByIdAndUpdate(
+    userId,
+    { $pull: { bookmarks: postId } },
+    { new: true }
+  );
+
+  return result;
+};
+
+const getUserFavorites = async (userId: string) => {
+  const result = await User.findById(userId).populate("bookmarks");
+  return result?.bookmarks;
+};
+
+export const UserServices = {
+  getSingleUserFromDB,
+  updateUserIntoDB,
+  followUser,
+  unfollowUser,
+  bookmarkPost,
+  removeBookmarkPost,
+  getUserFavorites
+}
